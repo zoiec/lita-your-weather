d82d48adda55373cf6ddd7e9a2b0b94a3fb71f39
Zoie Carnegie <zoie.carnegie@gmail.com>
2016-11-16 17:42:57 -0700

    Editing

--- lib/lita/handlers/your_weather.rb
+++ lib/lita/handlers/your_weather.rb
@@ -8,9 +8,17 @@ module Lita
       # API
       # https://api.apixu.com/v1/current.json?key=3c71764fa29246acaf164605161611&q=Paris
 
-      route(/^echo\s+(.+)/, :echo, command: true, help: {
+      route(/^weather\s+(.+)/, :weather_response, command: true, help: {
         "echo TEXT" => "Replies back with TEXT."
       })
+      
+      def weather_response(response)
+        
+        
+          http_response = http.get("https://api.apixu.com/v1/current.json?key=3c71764fa29246acaf164605161611",q: response.matches)
+          response.reply(http_response)
+      end
+      
     end
   end
 end
